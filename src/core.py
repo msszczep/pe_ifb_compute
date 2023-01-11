594875aa1c9549b3ea554c672ed2a77b539d8bee
Mitchell Szczepanczyk <mitchell@szcz.org>
2023-01-10 21:42:00 -0600

    Got generate_wc working

--- src/core.py
+++ src/core.py
@@ -2,19 +2,27 @@ from random import randrange
 
 number_of_products = 100
 
-def generate_wc_products(product_id):
-  return {"id": product_id, "supply": randrange(1, 10)}
+def generate_wc_product(product_id):
+    return {"id": product_id, "supply": randrange(1, 10)}
+
   
-def generate_cc_products(product_id):
-  return {"id": product_id, "demand": randrange(1, 10)}
+def generate_cc_product(product_id):
+    return {"id": product_id, "demand": randrange(1, 10)}
 
 
-# list(range(1, randrange(2, 10)))
+def generate_wc_products(num_of_products):
+    to_return = []
+    for i in list(range(1, num_of_products)):
+        to_return.append(generate_wc_product(i))
+    return to_return
+
 
 def generate_wc(wc_id, number_of_people, number_of_products):
   return {"id": wc_id,
           "population": number_of_people,
-          "products": map(generate_wc_products, list(range(1, number_of_products)))}
+          "products": generate_wc_products(number_of_products)}
+
+# list(range(1, randrange(2, 10)))
 
 #(defn generate-cc [cc-id number-of-people number-of-products]
 #  (hash-map :id cc-id
